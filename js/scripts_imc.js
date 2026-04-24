28d7963d7919974eb8cc277141fd6b657c3fa397
alifrebat <alifrebat@gmail.com>
2026-04-24 08:08:00 -0300

    Criado o array pessoas

--- js/scripts_imc.js
+++ js/scripts_imc.js
@@ -2,3 +2,7 @@
 const formImc = document.querySelector("#formulario")
 const btnEnviar = document.querySelector("#btn-enviar")
 const divLista = document.querySelector("#div-lista")
+
+//ARRAY PESSOA
+const pessoas = []
+
