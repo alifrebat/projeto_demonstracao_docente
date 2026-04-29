0efcefde6a2043cf8fd367830b1a5fa149bfea92
alifrebat <alifrebat@gmail.com>
2026-04-29 09:35:20 -0300

    Criado a função listarPessoa

--- js/scripts_imc.js
+++ js/scripts_imc.js
@@ -6,7 +6,7 @@ const btnEnviar = document.querySelector("#btn-enviar")
 const divLista = document.querySelector("#div-lista")
 
 //ARRAY PESSOA
-const pessoas = []
+let pessoas = []
 
 //CAPTURANDO O EVENTO DE CLICK NO BOTÃO ENVIAR
 btnEnviar.addEventListener('click', async (evt) => {
@@ -19,7 +19,7 @@ btnEnviar.addEventListener('click', async (evt) => {
         sexo: formPessoa.get('sexo'),
         data_nascimento: formPessoa.get('data-nascimento'),
         peso: formPessoa.get('peso'),
-        altura: formPessoa.get('altura')
+        altura: formPessoa.get('altura').replaceAll(',', '.')
     }
 
     const resultadoSalvar = await salvarDados(objPessoa)
@@ -27,3 +27,33 @@ btnEnviar.addEventListener('click', async (evt) => {
     formImc.reset()
 
 })
+
+//LISTA OS DADOS NA DIV LISTA
+const listarPessoa = async () => {
+    divLista.innerHTML = ''
+
+    pessoas =  await consultarPessoas()
+
+    pessoas.forEach((elem, i) => {
+        const divPessoa = document.createElement('div')
+        divPessoa.setAttribute('class', 'div-pessoa-item')
+        divPessoa.innerHTML = `<span> ${elem.idpessoa} - ${elem.nome} - ${elem.sexo} - ${elem.data_nascimento} - ${elem.peso} - ${elem.altura} - ${(parseFloat(elem.peso)/(parseFloat(elem.altura) * parseFloat(elem.altura))).toFixed(2).replaceAll('.',',')} - FALTA SITUAÇÃO </span>`
+
+        const btnExcluir = document.createElement('img')
+        btnExcluir.setAttribute('src', 'imagens/btn_excluir.png')
+        btnExcluir.setAttribute('alt', 'Excluir')
+        btnExcluir.setAttribute('title', 'Excluir')
+
+        btnExcluir.addEventListener('click',()=>{
+            alert('Recurso em construção')
+        })
+
+        divPessoa.appendChild(btnExcluir)
+
+        divLista.appendChild(divPessoa)
+        
+    })
+
+}
+
+listarPessoa()
